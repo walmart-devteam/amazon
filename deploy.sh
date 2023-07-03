da39fb2481d6a878212e9d9f28cf06bcbebf2efe
sandrarao <sandyrao888888@gmail.com>
2023-07-03 10:31:02 -0600

    commiting to local repo

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ This contains all deployment files
 This is a modified file
 All depoly scripts added
 This is for testing to see whether we can get back the files from staging to working area
+This is another important file
