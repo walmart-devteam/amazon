c2dff56ee9d5d61ac3e71fdc4dcaca2e0398c5c1
sandrarao <sandyrao888888@gmail.com>
2023-07-04 22:44:42 -0600

    moving to local repo deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@ This is a modified file
 All depoly scripts added
 This is for testing to see whether we can get back the files from staging to working area
 This is another important file
+This is the file i am using to practice 
+Added one line
